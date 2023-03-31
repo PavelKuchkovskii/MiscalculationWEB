7b7bc826a44bad64a02a3a90743087a6d6536f64
Pavel <pavel.kuchkovskii@gmail.com>
2023-03-31 16:47:54 +0300

    first commit

---
+++ src/main/java/org/euroholl/miscalculation/MiscalculationApplication.java
@@ -0,0 +1,13 @@
+package org.euroholl.miscalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiscalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/euroholl/miscalculation/MiscalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.euroholl.miscalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
